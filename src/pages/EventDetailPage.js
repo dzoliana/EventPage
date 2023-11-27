484a6abbb0c0ca3ec9103f670f0362e1954f4014
dzoliana <dzolicana@gmail.com>
2023-11-27 08:46:13 +0100

    Adding EventForm to DetailPage

--- src/pages/EventDetailPage.js
+++ src/pages/EventDetailPage.js
@@ -1,12 +1,12 @@
 /* import { useParams } from "react-router-dom"; */
-import { json, useLoaderData } from "react-router-dom";
+import { json, useRouteLoaderData } from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
   /*   const params = useParams();
    */
 
-  const data = useLoaderData();
+  const data = useRouteLoaderData("event-detail");
 
   return <EventItem event={data.event} />;
 }
