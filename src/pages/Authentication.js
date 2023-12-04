a1153de437e1c26af6b8c9744fa3e685ad8f913a
dzoliana <dzolicana@gmail.com>
2023-12-04 15:23:11 +0100

    Managing token expiration

--- src/pages/Authentication.js
+++ src/pages/Authentication.js
@@ -38,8 +38,11 @@ export async function action({ request }) {
 
   const resData = await response.json();
   const token = resData.token;
-  localStorage.setItem("token", token);
 
-  //soon: manage that token
+  localStorage.setItem("token", token);
+  const expiration = new Date();
+  expiration.setHours(expiration.getHours() + 1);
+  localStorage.setItem("expiration", expiration.toISOString());
+
   return redirect("/");
 }
